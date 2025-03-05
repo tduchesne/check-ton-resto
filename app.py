7c4eb7dbbc2b35374e760b82f285a66818e30b7d
Thierry Duchesne <thierryduchesne95@hotmail.com>
2025-03-05 14:42:17 -0500

    Init

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index(): 
+    return "Hi mom!"
---
+++ makefile
@@ -0,0 +1,7 @@
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+export FLASK_APP = app.py
+
+run:
+	flask run
+
